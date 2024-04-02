9176727931da1ed2910d4baac553882f20663302
Roman Lemeshov <stack.overflower01@gmail.com>
2024-04-02 14:44:09 +0300

    add transactions component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {MatButtonModule } from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import { MenuToolbarComponent } from './menu-toolbar/menu-toolbar.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuToolbarComponent,
+    TransactionsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule, 
+    MatFormFieldModule,
+    MatInputModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
